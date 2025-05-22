15db63571eccf55cc42afb196f878c8c8a1e0b7f
Manmohan <manu6520042@gmail.com>
2025-05-22 23:46:35 +0900

    finished hero, about me and skills sections

--- portfolio-frontend/src/components/ThemeToggle.jsx
+++ portfolio-frontend/src/components/ThemeToggle.jsx
@@ -1,8 +1,9 @@
 import { Moon, Sun } from "lucide-react";
 import { useEffect, useState } from "react";
+import { cn } from "@/lib/utils";
 
 export const ThemeToggle = () => {
-    const [isDarkMode, setIsDarkMode] = useState(false);
+    const [isDarkMode, setIsDarkMode] = useState(true);
     useEffect(() => {
         const storedTheme = localStorage.getItem("theme")
         if (storedTheme === "dark") {
@@ -26,7 +27,10 @@ export const ThemeToggle = () => {
     }
 
 
-    return <button onClick={toggleTheme}>
+    return <button onClick={toggleTheme}
+    className={cn("fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
+    "focus:outline-hidden"
+        )}>
         {isDarkMode? 
             (<Sun className="h-6 w-6 text-yellow-300"/>)
             : 
